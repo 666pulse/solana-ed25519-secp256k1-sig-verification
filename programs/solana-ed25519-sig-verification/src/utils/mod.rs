7be965c36b213e2ae7332ab01fb3849b84c38c83
liushooter <shooter.hero@gmail.com>
2024-03-04 21:30:45 +0800

    format

--- programs/solana-ed25519-sig-verification/src/utils/mod.rs
+++ programs/solana-ed25519-sig-verification/src/utils/mod.rs
@@ -2,7 +2,6 @@
 /// is constructed the way we expect. In this case, this is for
 /// `Ed25519Program.createInstructionWithPublicKey()` and
 /// `Secp256k1Program.createInstructionWithEthAddress()` instructions.
-
 pub mod ed25519;
 pub mod secp256k1;
 
