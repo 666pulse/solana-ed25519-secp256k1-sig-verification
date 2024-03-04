8d43676523276e5a4862815766a4a380e148b180
liushooter <shooter.hero@gmail.com>
2024-03-04 21:56:49 +0800

    secp256k1 verify

--- programs/solana-ed25519-sig-verification/src/lib.rs
+++ programs/solana-ed25519-sig-verification/src/lib.rs
@@ -5,9 +5,12 @@
 //!
 
 use anchor_lang::prelude::*;
+use anchor_lang::solana_program::{keccak, secp256k1_recover::secp256k1_recover};
+use libsecp256k1;
 use solana_program::instruction::Instruction;
 use solana_program::sysvar::instructions::{load_instruction_at_checked, ID as IX_ID};
 
+// use crate::error::ErrorCode;
 pub mod error;
 pub mod utils;
 
@@ -41,7 +44,7 @@ pub mod signatures {
     /// External instruction that only gets executed if
     /// a `Secp256k1Program.createInstructionWithEthAddress`
     /// instruction was sent in the same transaction.
-    pub fn verify_secp256k1(
+    pub fn secp256k1_verify_ins(
         ctx: Context<Verify>,
         eth_address: [u8; 20],
         msg: Vec<u8>,
@@ -58,6 +61,38 @@ pub mod signatures {
 
         Ok(())
     }
+
+    pub fn secp256k1_recover_ins(
+        _ctx: Context<Secp256k1Recover>,
+        args: Secp256k1RecoverArgs,
+    ) -> Result<()> {
+        let message_hash = {
+            let mut hasher = keccak::Hasher::default();
+            hasher.hash(&args.message);
+            hasher.result()
+        };
+
+        {
+            let signature = libsecp256k1::Signature::parse_standard_slice(&args.signature)
+                .map_err(|_| ProgramError::InvalidArgument)
+                .unwrap();
+
+            if signature.s.is_high() {
+                msg!("signature with high-s value");
+            }
+        }
+
+        let recovered_pubkey =
+            secp256k1_recover(&message_hash.0, args.recovery_id, &args.signature)
+                .map_err(|_| ProgramError::InvalidArgument)?;
+
+        require!(
+            recovered_pubkey.0 == args.public_key,
+            SigVeriryError::InvalidPublicKey
+        );
+
+        Ok(())
+    }
 }
 
 /// Context accounts
@@ -71,4 +106,25 @@ pub struct Verify<'info> {
     /// in the Anchor framework yet, so this is the safe approach.
     #[account(address = IX_ID)]
     pub ix_sysvar: AccountInfo<'info>,
+}
+
+#[derive(Accounts)]
+pub struct Secp256k1Recover<'info> {
+    #[account(mut)]
+    pub payer: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(AnchorDeserialize, AnchorSerialize, Clone)]
+pub struct Secp256k1RecoverArgs {
+    pub public_key: [u8; 64],
+    pub message: Vec<u8>,
+    pub signature: [u8; 64],
+    pub recovery_id: u8,
+}
+
+#[error_code]
+pub enum SigVeriryError {
+    #[msg("Publick key is invalid!")]
+    InvalidPublicKey,
 }
